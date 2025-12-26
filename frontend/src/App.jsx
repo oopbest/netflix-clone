dc66a6e3338d6e9229f9e7b0a258e400fdc8dfa4
Setthawut Pimthong <oop_best@yahoo.com>
2025-12-26 08:38:07 +0700

    feat: add WatchPage component for content playback and details display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser.js";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import WatchPage from "./pages/WatchPage.jsx";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -35,6 +36,10 @@ function App() {
         <Route
           path="/signup"
           element={!user ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/watch/:id"
+          element={user ? <WatchPage /> : <Navigate to="/login" />}
         />
       </Routes>
       <Footer />
---
+++ frontend/src/pages/WatchPage.jsx
@@ -0,0 +1,226 @@
+import { useEffect, useState, useRef } from "react";
+import useContentStore from "../store/content.js";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar.jsx";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import ReactPlayer from "react-player";
+import { ORIGINAL_IMG_BASE_URL, SMALL_IMG_BASE_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
+
+function WatchPage() {
+  const { id } = useParams();
+  const { contentType } = useContentStore();
+
+  const [trailers, setTrailers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [similarContent, setSimilarContent] = useState({});
+  const [contentDetails, setContentDetails] = useState(null);
+  const [currentTrailerIdx, setCurrentTrailerIdx] = useState(0);
+  const sliderRef = useRef(null);
+
+  useEffect(() => {
+    const getTrailers = async () => {
+      try {
+        const response = await axios.get(
+          `/api/v1/${contentType}/${id}/trailers`
+        );
+        setTrailers(response.data.trailers);
+      } catch (error) {
+        console.error("Error fetching trailers:", error);
+        setTrailers([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getTrailers();
+  }, [contentType, id]);
+  console.log("Trailers Data:", trailers);
+
+  useEffect(() => {
+    const getSimilarContent = async () => {
+      try {
+        const response = await axios.get(
+          `/api/v1/${contentType}/${id}/similar`
+        );
+        setSimilarContent(response.data.content);
+      } catch (error) {
+        console.error("Error fetching similar content:", error);
+        setSimilarContent({});
+      } finally {
+        setLoading(false);
+      }
+    };
+    getSimilarContent();
+  }, [contentType, id]);
+  console.log("Similar Content Data:", similarContent);
+
+  useEffect(() => {
+    const getContentDetails = async () => {
+      try {
+        const response = await axios.get(
+          `/api/v1/${contentType}/${id}/details`
+        );
+        setContentDetails(response.data.content);
+      } catch (error) {
+        console.error("Error fetching content details:", error);
+        setContentDetails(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getContentDetails();
+  }, [contentType, id]);
+  console.log("Content Details Data:", contentDetails);
+
+  const handlePreviousTrailer = () => {
+    setCurrentTrailerIdx((prevIdx) => (prevIdx > 0 ? prevIdx - 1 : prevIdx));
+  };
+
+  const handleNextTrailer = () => {
+    setCurrentTrailerIdx((prevIdx) =>
+      prevIdx < trailers.length - 1 ? prevIdx + 1 : prevIdx
+    );
+  };
+
+  const formatReleaseDate = (date) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(date).toLocaleDateString(undefined, options);
+  };
+
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  return (
+    <div className="bg-black min-h-screen text-white">
+      <div className="mx-auto container px-4 py-8 h-full">
+        <Navbar />
+
+        {trailers.length > 0 && (
+          <div className="flex justify-between items-center mb-4">
+            <button
+              className={`bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${
+                currentTrailerIdx === 0 ? "opacity-50 cursor-not-allowed" : ""
+              }`}
+              onClick={handlePreviousTrailer}
+              disabled={currentTrailerIdx === 0}
+            >
+              <ChevronLeft size={24} />
+            </button>
+            <button
+              className={`bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${
+                currentTrailerIdx === trailers.length - 1
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              }`}
+              onClick={handleNextTrailer}
+              disabled={currentTrailerIdx === trailers.length - 1}
+            >
+              <ChevronRight size={24} />
+            </button>
+          </div>
+        )}
+
+        <div className="aspect-video mb-8 p-2 sm:px-10 md:px-32">
+          {trailers.length > 0 && (
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${trailers[currentTrailerIdx].key}`}
+              width={"100%"}
+              height={"70vh"}
+              controls
+              className="mx-auto overflow-hidden rounded-lg"
+            />
+          )}
+
+          {trailers.length === 0 && (
+            <p className="text-center text-2xl font-bold">
+              No trailers available for{" "}
+              <span className="font-bold text-red-600">
+                {contentDetails?.title || contentDetails?.name}
+              </span>
+            </p>
+          )}
+        </div>
+
+        {/* movie details */}
+        <div className="flex flex-col md:flex-row items-center justify-between gap-20 max-w-6xl mx-auto">
+          <div className="mb-4 md:mb-0">
+            <h2 className="text-5xl font-bold text-balance">
+              {contentDetails?.title || contentDetails?.name}
+            </h2>
+            <p className="mt-2 text-lg">
+              {formatReleaseDate(
+                contentDetails?.release_date || contentDetails?.first_air_date
+              )}{" "}
+              |{" "}
+              {contentDetails?.adult ? (
+                <span className="text-red-600">18+</span>
+              ) : (
+                <span className="text-green-600">PG-13</span>
+              )}
+            </p>
+            <p className="mt-4 text-lg">{contentDetails?.overview}</p>
+          </div>
+          <img
+            src={ORIGINAL_IMG_BASE_URL + contentDetails?.poster_path}
+            alt="Poster image"
+            className="max-h-[600px] rounded-md"
+          />
+        </div>
+
+        {/* similar content */}
+        {similarContent.length > 0 && (
+          <div className="mt-12 max-w-5xl mx-auto relative">
+            <h3 className="text-3xl font-bold mb-4">Similar Movies/Tv Show</h3>
+            <div
+              className="flex overflow-x-scroll scrollbar-hide gap-4 pb-4 group"
+              ref={sliderRef}
+            >
+              {similarContent.map((item) => (
+                <Link
+                  to={`/watch/${item.id}`}
+                  key={item.id}
+                  className="w-52 flex-none"
+                >
+                  <img
+                    src={SMALL_IMG_BASE_URL + item.poster_path}
+                    alt="Poster path"
+                    className="w-full h-auto rounded-md"
+                  />
+                  <h4 className="mt-2 text-lg font-semibold">
+                    {item.title || item.name}
+                  </h4>
+                </Link>
+              ))}
+
+              <ChevronRight
+                className="absolute top-1/2 -translate-y-1/2 right-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all durtation-300 cursor-pointer bg-red-600 text-white rounded-full"
+                onClick={scrollRight}
+              />
+              <ChevronLeft
+                className="absolute top-1/2 -translate-y-1/2 left-2 w-8 h-8 opacity-0 group-hover:opacity-100 transition-all durtation-300 cursor-pointer bg-red-600 text-white rounded-full"
+                onClick={scrollLeft}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default WatchPage;
